2406368c0d530668cf776671b0671889e2e00e15
ernchenghoo <ernchenghoo2@gmail.com>
2018-10-20 20:22:48 +0800

    testing scene with scala

--- src/main/scala/mainClass.scala
+++
@@ -1,7 +0,0 @@
-package OOP
-
-object mainApp extends App {
-  val tiger = new Mammal (true, "tiger", Features(4, 50, 40))
-  
-  println (tiger.animalFeatures)
-}
---
+++ src/main/scala/mainSystem/MainApp.scala
@@ -0,0 +1,30 @@
+package mainSystem
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.Includes._
+import scalafxml.core.{NoDependencyResolver, FXMLView, FXMLLoader}
+import javafx.{scene => jfxs}
+
+object MainApp extends JFXApp {
+  
+  val rootResource = getClass.getResource("/scenes/RootLayout.fxml")
+  val loader = new FXMLLoader(rootResource, NoDependencyResolver)
+  loader.load();
+  val roots = loader.getRoot[jfxs.layout.BorderPane]
+  
+  stage = new PrimaryStage {
+    title = "AddressApp"
+    scene = new Scene {
+      root = roots
+    }
+  }
+  def showPersonOverview() = {
+    val resource = getClass.getResource("/scenes/PersonOverview.fxml")
+    val loader = new FXMLLoader(resource, NoDependencyResolver)
+    loader.load();
+    val roots = loader.getRoot[jfxs.layout.AnchorPane]
+    this.roots.setCenter(roots)
+  } 
+  showPersonOverview()
+}
