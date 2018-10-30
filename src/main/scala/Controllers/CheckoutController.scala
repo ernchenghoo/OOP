0ad6426bc63cf5634736bb1142ea2cb0b6ff76b2
ernchenghoo <ernchenghoo2@gmail.com>
2018-10-30 22:31:17 +0800

    new check item id with database function

--- src/main/scala/Controllers/CheckoutController.scala
+++ src/main/scala/Controllers/CheckoutController.scala
@@ -12,22 +12,24 @@ import scalafxml.core.{NoDependencyResolver, FXMLView, FXMLLoader}
 class CheckoutController (
 	private val connectDbButton: Button,
 	private val checkout2: Button,
-	private val firstID : Label,    
-    private val firstName : Label,  
+	private val firstID : Label,
+    private val firstName : Label,
     private val firstPrice : Label,
     private val secondID : Label,
     private val secondName :  Label,
-    private val secondPrice : Label  
+    private val secondPrice : Label,
+    private val findItemTextbox: TextField
 	) {	
 		val inventoryDBClass = new InventoryDB() 
 
 		def moveToMainMenu() = {
 			MainApp.showPersonOverview()
 		}
-
-		//testing
-		def connectDB() = {
-			inventoryDBClass.connectToDB()
+		def checkoutItem = {			
+			var itemExists = inventoryDBClass.checkItemExistence(findItemTextbox.text.value)
+			if (itemExists)
+				firstID.setVisible(true)
+				firstID.text.value = findItemTextbox.text.value
 		}
 
 
--- src/main/scala/Database/InventoryDB.scala
+++ src/main/scala/Database/InventoryDB.scala
@@ -1,29 +1,32 @@
 package Database
 import java.sql.{Connection,DriverManager}
+import scala.collection.mutable.Set
 
 class InventoryDB{   
 
     val myDatabaseDetails = new myDBDetails
 
-    def connectToDB() {
-        try {
-            Class.forName(myDatabaseDetails.driver)
-            myDatabaseDetails.connection = DriverManager.getConnection(myDatabaseDetails.url, myDatabaseDetails.username, 
-                myDatabaseDetails.password)
-            
-            val statement = myDatabaseDetails.connection.createStatement
-            val rs = statement.executeQuery("select * from inventory")
-            while (rs.next) {
-                var name = rs.getString("name")
-                var id = rs.getInt("id")
-                var price = rs.getFloat("price")
-                println(name, id, price)
-            }
-            println("connection successful")
-        } 
-        catch {
-            case e: Exception => e.printStackTrace
-        }
-        myDatabaseDetails.connection.close
-    }   
+    try {
+        Class.forName(myDatabaseDetails.driver)
+        myDatabaseDetails.connection = DriverManager.getConnection(myDatabaseDetails.url, myDatabaseDetails.username,
+            myDatabaseDetails.password)
+        println("connection successful")
+    }
+    catch {
+        case e: Exception => e.printStackTrace
+    }
+
+    val statement = myDatabaseDetails.connection.createStatement
+    val rs = statement.executeQuery("select * from inventory")
+    var idSet = Set[String]()
+    while (rs.next) {
+         var id = rs.getInt("id").toString
+         idSet.add(id)
+    }
+
+    def checkItemExistence(ID: String): Boolean = {
+        idSet.contains(ID)
+    }
+
+    myDatabaseDetails.connection.close
 }
