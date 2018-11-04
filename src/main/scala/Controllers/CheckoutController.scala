682b60175a8d8437e67f80815a306f41044ac330
ernchenghoo <ernchenghoo2@gmail.com>
2018-11-04 21:33:43 +0800

    checkout item function

--- src/main/scala/Controllers/CheckoutController.scala
+++ src/main/scala/Controllers/CheckoutController.scala
@@ -1,5 +1,5 @@
 package Controllers
-import Database.InventoryDB
+import Database.myDBDetails
 import MainSystem.MainApp
 import scalafx.scene.control._
 import scalafx.scene.layout._
@@ -8,29 +8,66 @@ import scalafx.event._
 import scalafx.beans.property.{StringProperty}
 import javafx.{scene => jfxs}
 import scalafxml.core.{NoDependencyResolver, FXMLView, FXMLLoader}
+import java.sql.{Connection,DriverManager}
+import scalafx.scene.control.Alert.AlertType
+
 
 @sfxml
 class CheckoutController (
 	private val connectDbButton: Button,
 	private val checkout2: Button,	
-    private val findItemTextbox: TextField,
+    private val searchItemID: TextField,
+    private val quantityField: TextField,
     private val checkoutGrid: GridPane,
 	) {	
-		val inventoryDBClass = new InventoryDB() 
+		//establish database connection when enter checkout page
+		Class.forName(myDBDetails.driver)
+		myDBDetails.connection = DriverManager.getConnection(myDBDetails.url, myDBDetails.username, myDBDetails.password)
+		val statement = myDBDetails.connection.createStatement
+		var itemRow: Int = 1					
 
 		def moveToMainMenu() = {
 			MainApp.showPersonOverview()
+			myDBDetails.connection.close
+		}
+		//create new column in grid pane and add labels containing all item information
+		def addCheckoutItem (item: Array[Any]) {
+			checkoutGrid.addRow(itemRow)
+			var lineAmount: Double = item(2).toString.toDouble * quantityField.text.value.toDouble			
+			checkoutGrid.add (new Label (item(1).toString),0,itemRow)
+			checkoutGrid.add (new Label(item(0).toString),1,itemRow)
+			checkoutGrid.add (new Label(item(2).toString),2,itemRow)
+			checkoutGrid.add (new Label(quantityField.text.value.toString),3,itemRow)
+			checkoutGrid.add (new Label(lineAmount.toString),4,itemRow)	
+			itemRow += 1		
 		}
 		
-		def checkoutItem = {				
-			var itemExists = inventoryDBClass.checkItemExistence(findItemTextbox.text.value)						
-			if (itemExists) {
-				checkoutGrid.add(new Label(findItemTextbox.text.value),0,1)
-			}
-		}
-		
+		def checkoutItem = {	
+			//query for comparing item in database			
+			val queryValue = statement.executeQuery("select * from inventory where id = '"+ searchItemID.text.value+ "'" )			
+			
+			//if item found in database
+		    if (queryValue.next && quantityField.text.value.nonEmpty ) {
+				var checkoutItem = Array (queryValue.getString("name"), queryValue.getInt("id"), queryValue.getDouble("price"))
+				addCheckoutItem(checkoutItem)
+		    }
+		    //if either textfield is empty
+		    else if (searchItemID.text.value.isEmpty || quantityField.text.value.isEmpty ){
+		        val emptyAlert = new Alert(AlertType.Warning){
+		          initOwner(MainApp.stage)
+		          title       = "No Selection"
+		          headerText = "Empty Field"
+		          contentText  = "Input value incomplete"
+		        }.showAndWait()
+		    }	
+		    else {
+				  val NotExistAlert = new Alert(AlertType.Warning){
+		          initOwner(MainApp.stage)
+		          title       = "Not Found"
+		          headerText = "Item Not Found"
+		          contentText  = "Item does not exist"
+		        }.showAndWait()
+		    }	
 
-
-
-
+		}		
 }	
--- src/main/scala/Models/Inventory.scala
+++ src/main/scala/Models/Inventory.scala
@@ -1,6 +1,8 @@
 package Models 
 
+
 class Inventory(var name: String, val id: Int, var price: Double) {	
+	
 	
 	
 }
